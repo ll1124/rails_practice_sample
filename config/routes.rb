32e4440af13de911c79f8842f0393532c3ef553e
jin <ll1124@hanmail.net>
2017-02-25 23:26:39 +0900

    연습0

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get '/posts' => 'posts#index'
   resources :posts
+  resources :home
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
