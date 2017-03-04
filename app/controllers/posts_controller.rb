53ea9b6e70a37b098bb370f94e8f31d1e5ea6cb0
jin <ll1124@hanmail.net>
2017-03-04 17:15:49 +0900

    minor change

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :email, :phone, :name)
+      params.require(:post).permit(:title, :content, :name, :phone, :email)
     end
 end
